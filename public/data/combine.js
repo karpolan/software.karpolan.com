87d60d9cba67aebcfea297d0002d8822d36a3dbb
Anton Karpenko <karpolan@karpolan.com>
2019-04-14 23:45:08 +0300

    SEO files added. Builder for index and sitemap added

--- public/data/combine.js
+++ public/data/combine.js
@@ -4,6 +4,18 @@
 const glob = require('glob');
 const fs = require('fs');
 
+/**
+ * Returns capitalized version (Upper first char for every word) of the given string
+ * @param {String} s - String To Capitalize
+ */
+const capitalizeString = (s) => {
+//	return s.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+	return s.replace(/(?:^|\s)\S/g, (a) => a.toUpperCase() );
+};
+
+/**
+ * Build "all.json" file with array of all "/abc-xyz/schema.json" files
+ */
 glob('**/schema.json', {}, function (err, files) {
 	let allObjects = [];
 
@@ -33,5 +45,66 @@ glob('**/schema.json', {}, function (err, files) {
 
 	console.log('All "schema.json" files sucessfuly combined into "all.json" file');
  	return true;
-}); // glob()
+}); // glob('**/schema.json')
+
+
+/**
+ * Build index.html and sitemap.xml files with links to all "/abc-xyz/" folders 
+ */
+glob('**/index.html', {}, function (err, files) {
+	const htmlItems = [];
+	const xmlItems = [];
+
+	if (err) {
+		console.error('glob error: ', err);
+		return false;
+	}
+
+	files.map((fileName, index) => {
+		console.log('Linking "%s" file...', fileName);
+		const link = fileName.substring(0, fileName.indexOf("/"));
+		if (link !== '') { // Skipping root folder
+			const text = capitalizeString( link.replace(/-/g, ' ') );
+			htmlItems.push(`<li><a href="${link}/">${text}</a></li>`);
+			xmlItems.push(`<url><loc>https://software.karpolan.com/data/${link}/</loc><priority>0.4</priority></url>`);
+		}
+	}); // files.map()
+
+
+	// Write "index.html"
+	const htmlContent = `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1 id="name">All Products</h1>
+	<ul>
+${htmlItems.join('\n')}
+	</ul>
+</body>
+</html>`;
+	fs.writeFile('index.html', htmlContent, function(err) {
+		if (err) {
+				console.error('fs.writeFile error: ', err);
+				return false;
+		}
+	});
+	console.log('HTML links to all sub-folders added to "index.html" file');
+
+	// Write "sitemap.xml"
+	const xmlContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${xmlItems.join('\n')}
+</urlset>`
+	fs.writeFile('sitemap.xml', xmlContent, function(err) {
+		if (err) {
+				console.error('fs.writeFile error: ', err);
+				return false;
+		}
+	});
+	console.log('URL of all sub-folders added to "sitemap.xml" file');
+
+	return true;
+}); // glob('**/index.html')
 
