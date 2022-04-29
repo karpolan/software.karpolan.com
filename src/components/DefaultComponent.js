50f5a3b50d2e9334aed9a802fff2b72a24723923
KARPOLAN <karpolan@karpolan.com>
2022-04-29 03:32:52 +0200

    feat: Magnifying Tools product fix: typos update: npm versions

--- src/components/DefaultComponent.js
+++ src/components/DefaultComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /**
- * Renders the Boilerplate for new conpomnents
+ * Renders the Boilerplate for new components
  */
 function NotImplemented() {
   return <div className="alert alert-warning">This component is not implemented yet...</div>;
